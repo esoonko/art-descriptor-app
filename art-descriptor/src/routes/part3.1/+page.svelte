5dab7d7a4b1fccf205b30ceb559c6dc38b98136b
Esoon Ko <esoonko@gmail.com>
2025-04-20 10:13:41 +0200

    Consolidate to prod code.

--- art-descriptor/src/routes/part3.1/+page.svelte
+++ art-descriptor/src/routes/part3.1/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import GenerateText from '../../lib/components/generate-text.svelte';
 
-  const page = '3';  // This can be dynamic based on your page logic
-  const scrollarea = "font-sans text-[rgb(27,18,18)] text-justify mt-[80px] ml-[-80px] tracking-wide leading-custom font-normal px-[5px] w-[calc(100%-100px)] text-[11px]"
-  const divclass = "font-sans text-[rgb(27,18,18)] text-justify mt-[80px] ml-[-5px] tracking-wide leading-custom font-normal px-[5px] w-[calc(100%-100px)] text-[11px]"
-  const historydivclass = "font-sans text-[rgb(27,18,18)] text-justify mt-[60px] ml-[-5px] tracking-wide leading-custom font-normal px-[5px] w-[calc(100%-100px)] text-[11px]"
+  const page = '3';
+  const scrollarea = "font-sans text-[rgb(27,18,18)] text-justify mt-[70px] ml-[-60px] tracking-wide leading-custom font-normal px-[5px] w-[calc(100%-100px)] text-[11px]"
+  const divclass = "font-sans text-[rgb(27,18,18)] text-justify mt-[10px] ml-[-5px] tracking-wide leading-custom font-normal px-[5px] text-[11px]"
+  const historydivclass = "font-sans text-[rgb(27,18,18)] text-justify mt-[20px] ml-[-5px] tracking-wide leading-custom font-normal px-[5px] text-[11px]"
+  const timedivclass = "font-sans text-[rgb(27,18,18)] text-justify mb-[40px] ml-[-5px] tracking-wide leading-custom font-normal px-[5px] text-[11px]"
+  const buttonclass = "font-sans text-[rgb(27,18,18)] text-justify mt-[75px] ml-[-5] tracking-wide leading-custom font-normal px-[5px] text-[11px]"
 </script>
 
-<GenerateText {page} {divclass} {scrollarea} {historydivclass}/>
+<GenerateText {page} {divclass} {scrollarea} {historydivclass} {buttonclass} {timedivclass}/>
